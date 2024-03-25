f0c82847a0323de5c122c6c2faec1a3d7a53b118
abidakosar <161754486+abidakosar@users.noreply.github.com>
2024-03-24 17:45:57 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+console.log(randomNumber);
+
+const answers = await inquirer.prompt([
+    {
+
+name: "userGuessdNumber",
+type: "number",
+message: "Please guess a number between 1-6:",
+    },
+]);
+if(answers.userGuessdNumber === randomNumber){
+    console.log("congratulation! you guessed right number")
+}else{
+    console.log("you guess wrong number")
+}
